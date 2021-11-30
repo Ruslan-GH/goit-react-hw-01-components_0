e8a0b6b4a5c8bd28089a24ef0c33ef9973e003e7
Ruslan-GH <82412111+Ruslan-GH@users.noreply.github.com>
2021-11-30 23:30:50 +0200

    add Profile and prop-types

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import Profile from './components/Profile';
+import user from './social-profile/user.json';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Profile
+        username={user[0].username}
+        tag={user[0].tag}
+        location={user[0].location}
+        avatar={user[0].avatar}
+        stats={user[0].stats}
+      />
     </div>
   );
 }
-
-export default App;
